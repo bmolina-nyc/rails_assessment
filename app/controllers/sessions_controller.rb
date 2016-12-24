cf2295cc4872905536d45def3201d1e32f06780a
bmolina-nyc <bruce.d.molina@gmail.com>
2016-12-23 23:18:10 -0500

    added devise and omniauth

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,8 @@
+class SessionsController < ApplicationController
+  
+
+  def destroy
+    session.clear
+    redirect_to '/'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  get 'static/home'
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  root 'static#home'
+
+  post "sessions/destroy" => "sessions#destroy", as: :logout
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
